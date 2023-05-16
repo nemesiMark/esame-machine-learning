07f5cec259b37812f5ad1fa3c380e638bb7fd562
nemesiMark <marcantonio.cilia@gmail.com>
2023-05-16 11:15:24 +0200

    input X_test

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def main():
     st.write("Y_PRED: ",round(new_model.predict([[indus,nox,rm,tax,pratio,lstat]])[0],2))
 
     # carico file csv
-    uploaded_file = st.file_uploader("Choose your file CSV with this COLUMNS: (crim, zn, indus, chas, nox, rm, age, dis, rad, tax, pratio, b, lstat, medv)")
+    uploaded_file = st.file_uploader("Choose your file CSV: NEED output.csv")
     
     # verifico che il file non sia vuoto
     if uploaded_file is not None:
@@ -49,30 +49,11 @@ def main():
             # leggo il csv e splitto in X e y 
             df = pd.read_csv(uploaded_file)
 
-            df = df.rename(columns={'medv': 'price'})
-            X = df.drop(columns=['crim','zn','chas','age','dis','rad','b', 'price'])
-            y = df['price']
-
-            # splitto parte del dataframe per avere una parte di train e un' altra di test
-            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 667)
-            y_pred = new_model.predict(X_test)
+            y_pred = new_model.predict(df)
 
             # creo il dataframe da scaricare con X_test e aggiungendo una nuova colonna "predict" y_pred
-            df1 = X_test
+            df1 = df
             df1["Predict"] = y_pred
-
-            # calcolo degli errori
-            r2score = r2_score(y_test, y_pred)
-            mae = mean_absolute_error(y_test, y_pred)
-            mse = mean_squared_error(y_test, y_pred)
-            rmse = mean_squared_error(y_test, y_pred, squared=False)
-
-            # visualizzo gli errori precedentemente calcolati
-            st.header("Calcolo errori")
-            st.write('R2_score: ', r2score)
-            st.write('MAE: ', mae)
-            st.write('MSE: ', mse)
-            st.write('RMSE: ', rmse)
 
 
             # bottone per scaricare X_test + y_pred
